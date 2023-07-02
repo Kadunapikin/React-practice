235ac78c8b216c01ab6da49969bf36f117ffc288
kadunapikin <ibakoshi27@gmail.com>
2023-07-02 22:32:25 +0100

    create objejct properties to set attributes in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
-// How to list elements using .map in JSX
 function App() {
+  // How to list elements using .map in JSX
   // one
   const strings = ['Home', 'Shop', 'About Me'];
   const listItems = strings.map(string => <li>{string}</li>);
@@ -15,11 +15,44 @@ function App() {
   const people = ['Rowe', 'Prevost', 'Gare'];
   const peopleList = people.map((person, i) => <li key={'person_' + i}>{person}</li>);
 
+  // using objejct properties to set attributes in react
+  const topStudents = {
+    bobai : "senior-developer",
+    david : "junior-developer",
+    winnie : "senior-developer"
+  };
+
+  const bobai = (
+    <div>
+      <h1>{topStudents.bobai}</h1>
+      <p>Bobai is one of the best students at TOP school</p>
+    </div>
+  );
+
+  const david = (
+    <div>
+      <h1>{topStudents.david}</h1>
+      <p>David is one of the usal absenties at TOP school</p>
+    </div>
+  );
+
+  const studentList = (
+    <ol>
+      <li>{bobai}</li>
+      <li>{david}</li>
+    </ol>
+  );
+
   return (
     <div className="App">
-      <ul>{listItems}</ul>
-      <ul>{liArray}</ul>
-      <ol>{peopleList}</ol>
+      <div>
+        <ul>{listItems}</ul>
+        <ul>{liArray}</ul>
+        <ol>{peopleList}</ol>
+      </div>
+      <div>
+        <ul>{studentList}</ul>
+      </div>
     </div>
   );
 }
