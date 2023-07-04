60bf304e640c61d2d9f66e1fd443ec963e7d51ed
kadunapikin <ibakoshi27@gmail.com>
2023-07-04 23:37:37 +0100

    add EventListeners to dayTwo.js

--- my-app/src/practice/daytwo.js
+++ my-app/src/practice/daytwo.js
@@ -8,9 +8,25 @@ const owl = {
     src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg"
 };
 
+// How to create EventListeners
+function handleClick() {
+    alert('Submission successful, return to home page.');
+}
+
+function handleHover() {
+    alert('Stop it, no dey hoover over me biko.')
+}
+
     return (
     <div>
         <div className='first-div'>
+            <h1>Result Submission Button</h1>
+            <button onClick={handleClick}>Submit Result</button>
+        </div>
+        <div onMouseOver={handleHover}>
+            <p>I'm the second div that don't like people hoovering over me.</p>
+        </div>
+        <div className='third-div'>
             <h1>{owl.title}</h1>
             <img
             src={owl.src}
