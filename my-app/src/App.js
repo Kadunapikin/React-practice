f6864612f655b53ebb494818d360dd9242d27179
kadunapikin <ibakoshi27@gmail.com>
2023-07-05 16:20:14 +0100

    create props.js and add props to App component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import DayOne from './practice/dayone';
 import DayTwo from './practice/daytwo';
+import Player from './practice/props';
+
 
 function App() {
   return (
     <>
     <DayOne />
     <DayTwo />
+    <Player songName="Live Your Life" artist="Rihana" />
     </>
   );
 }
