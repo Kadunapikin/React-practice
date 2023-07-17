ef145e22f7919fbf5c257ed07dcd7d749008c802
kadunapikin <ibakoshi27@gmail.com>
2023-07-17 19:39:52 +0100

    Refactor ColorPicker.js component

--- my-app/src/practice/ColorPicker.js
+++ my-app/src/practice/ColorPicker.js
@@ -1,7 +1,27 @@
 // import the default export and the named export `useState` from the 'react' library
 import React, { useState } from 'react';
 
+// export default function ColorPicker() {
+//   // call useState and assign its return values to `color` and `setColor`
+//   const [color, setColor] = useState();
+
+//  const divStyle = {backgroundColor: color};
+
+//   return (
+//     <div style={divStyle}>
+//       <p>The color is {color}</p>
+//       {}
+//       <button onClick={() => setColor('Aquamarine')}>Aquamarine</button>
+//       <button onClick={() => setColor('BlueViolet')}>BlueViolet</button>
+//       <button onClick={() => setColor('Chartreuse')}>Chartreuse</button>
+//       <button onClick={() => setColor('CornflowerBlue')}>CornflowerBlue</button>
+//     </div>
+//   );
+// }
+
+// Color picker App can be refactor like this
 export default function ColorPicker() {
+    const diffColors = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'Yellow', 'Red', 'Black', 'CornflowerBlue']
   // call useState and assign its return values to `color` and `setColor`
   const [color, setColor] = useState();
 
@@ -9,11 +29,10 @@ export default function ColorPicker() {
 
   return (
     <div style={divStyle}>
-      <p>The color is {color}</p>
-      <button onClick={() => setColor('Aquamarine')}>Aquamarine</button>
-      <button onClick={() => setColor('BlueViolet')}>BlueViolet</button>
-      <button onClick={() => setColor('Chartreuse')}>Chartreuse</button>
-      <button onClick={() => setColor('CornflowerBlue')}>CornflowerBlue</button>
+      <p>The selected color is {color}</p>
+      {diffColors.map((diffColor) => (
+        <button onClick={() => setColor(diffColor)} key={diffColor}>{diffColor}</button>
+      ))}
     </div>
   );
 }
